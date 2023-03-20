ae5f32fd7639222b35eaf92f95e1eb5ed28dcace
karim2424 <karimkarimkarim1997@gmail.com>
2023-03-20 12:28:45 +0100

    some testing

--- QuizApp/main.js
+++ QuizApp/main.js
@@ -1,17 +1,43 @@
-function getDataNonAsync() {
+function getData() {
   fetch("QandA.json")
     .then((response) => response.json())
     .then((result) => {
-      return result["answers"];
+      return result;
     })
-    .then((re) => {
-      console.log(re[2]);
+    .then((jsonResult) => {
+      // let myCategory = Object.keys(jsonResult)
+      let randomQuetsion = Math.floor(
+        Math.random() * jsonResult["questions"].length
+      );
+      let randomAnswer = Math.floor(
+        Math.random() * jsonResult["answers"].length
+      );
+      for (let i = 0; i < 3; i++) {
+        let randomNumber = Math.floor(
+          Math.random() * jsonResult["answers"].length
+        );
+        if (randomAnswer === randomNumber) {
+          continue;
+        } else {
+          let select = document.createElement("select");
+          let option = document.createElement("option");
+          let optionValue = document.createAttribute("value");
+          let optionText = document.createTextNode(
+            jsonResult["answers"][randomNumber]
+          );
+          optionValue.value = jsonResult["answers"][randomNumber];
+          option.setAttributeNode(optionValue);
+          option.appendChild(optionText);
+          select.appendChild(option);
+          document.querySelector("body").appendChild(select);
+        }
+      }
     })
     .catch((error) => {
       console.log(error);
     });
 }
-getDataNonAsync();
+getData();
 // async function getData() {
 //   try {
 //     let result = await fetch("QandA.json");
