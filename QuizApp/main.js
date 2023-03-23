11ba7acbf2482b622f9554e268b56cbb4c347413
karim2424 <karimkarimkarim1997@gmail.com>
2023-03-23 10:05:21 +0100

    taking a different approach

--- QuizApp/main.js
+++ QuizApp/main.js
@@ -1,3 +1,5 @@
+let myQuestion = document.querySelector(".quiz-one .question");
+let myAnswers = document.querySelectorAll(".quiz-one input");
 function getData() {
   fetch("QandA.json")
     .then((response) => response.json())
@@ -8,36 +10,20 @@ function getData() {
       let randomQuetsion = Math.floor(
         Math.random() * jsonResult["questions"].length
       );
+      myQuestion.innerHTML = jsonResult["questions"][randomQuetsion];
       let randomAnswer = Math.floor(
         Math.random() * jsonResult["answers"].length
       );
+      myAnswers[randomAnswer].innerHTML = jsonResult["answers"][randomQuetsion];
       for (let i = 0; i < 3; i++) {
         let randomNumber = Math.floor(
           Math.random() * jsonResult["answers"].length
         );
-        if (randomAnswer === randomNumber) {
+        if (randomQuetsion === randomNumber) {
           continue;
         } else {
-          let input = document.createElement("input");
-          let lable = document.createElement("lable");
-          let inputValue = document.createAttribute("value");
-          inputValue.value = jsonResult["answers"][randomNumber];
-          input.setAttributeNode(inputValue);
-          let inputType = document.createAttribute("type");
-          inputType.value = "checkbox";
-          input.setAttributeNode(inputType);
-          let inputName = document.createAttribute("name");
-          inputName.value = "answers";
-          input.setAttributeNode(inputName);
-          let lableFor = document.createAttribute("for");
-          lableFor.value = jsonResult["answers"][randomNumber];
-          lable.setAttributeNode(lableFor);
-          let lableText = document.createTextNode(
-            jsonResult["answers"][randomNumber]
-          );
-          lable.appendChild(lableText);
-          document.body.appendChild(input);
-          document.body.appendChild(lable);
+          if ((myAnswers[randomNumber].innerHTML = "")) {
+          }
         }
       }
     })
@@ -59,3 +45,24 @@ getData();
 // getData();
 
 // some changes
+
+// let input = document.createElement("input");
+// let lable = document.createElement("lable");
+// let inputValue = document.createAttribute("value");
+// inputValue.value = jsonResult["answers"][randomNumber];
+// input.setAttributeNode(inputValue);
+// let inputType = document.createAttribute("type");
+// inputType.value = "checkbox";
+// input.setAttributeNode(inputType);
+// let inputName = document.createAttribute("name");
+// inputName.value = "answers";
+// input.setAttributeNode(inputName);
+// let lableFor = document.createAttribute("for");
+// lableFor.value = jsonResult["answers"][randomNumber];
+// lable.setAttributeNode(lableFor);
+// let lableText = document.createTextNode(
+//   jsonResult["answers"][randomNumber]
+// );
+// lable.appendChild(lableText);
+// document.body.appendChild(input);
+// document.body.appendChild(lable);
