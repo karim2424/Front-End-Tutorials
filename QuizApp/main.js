670bb9a4a0a5439bfe6df27192853e3028e30082
karim2424 <karimkarimkarim1997@gmail.com>
2023-04-04 14:48:14 +0100

    final tuch

--- QuizApp/main.js
+++ QuizApp/main.js
@@ -6,46 +6,62 @@ let buttonResult = document.querySelector(".my-button2");
 let mySet = new Set();
 let myPickedAnswer;
 let theRightAnswer;
-let count = 0;
+let rightCount = 0;
+let wrongCount = 0;
 function getData() {
   fetch("QandA.json")
     .then((response) => response.json())
     .then((result) => {
       return result;
     })
-    .then((jsonResult) => {
-      let myQuestionArr = jsonResult;
-      test = jsonResult;
-      let randomQuestion = Math.floor(Math.random() * myQuestionArr.length);
-      myQuestion.innerHTML = myQuestionArr[randomQuestion]["title"];
-      while (mySet.size < 4) {
-        let randomNumber = Math.floor(Math.random() * 4);
-        mySet.add(randomNumber);
-      }
-      let myArr = Array.from(mySet);
-      for (let i = 0; i < myAnswersLabel.length; i++) {
-        myAnswersLabel[myArr[i]].innerHTML =
-          myQuestionArr[randomQuestion][`asw${i + 1}`];
-        myAnswers[myArr[i]].setAttribute(
-          "value",
-          myQuestionArr[randomQuestion][`asw${i + 1}`]
-        );
-      }
-      theRightAnswer = myQuestionArr[randomQuestion]["rAsw"];
-      console.log(myQuestionArr);
+    .then(
+      (jsonResult) => {
+        // ********************************************* Create first question *********************************************
+        let myQuestionArr = jsonResult;
+        test = jsonResult;
+        let randomQuestion = Math.floor(Math.random() * myQuestionArr.length);
+        myQuestion.innerHTML = myQuestionArr[randomQuestion]["title"];
+        while (mySet.size < 4) {
+          let randomNumber = Math.floor(Math.random() * 4);
+          mySet.add(randomNumber);
+        }
+        let myArr = Array.from(mySet);
+        for (let i = 0; i < myAnswersLabel.length; i++) {
+          myAnswersLabel[myArr[i]].innerHTML =
+            myQuestionArr[randomQuestion][`asw${i + 1}`];
+          myAnswers[myArr[i]].setAttribute(
+            "value",
+            myQuestionArr[randomQuestion][`asw${i + 1}`]
+          );
+        }
+        // ********************************************* End Create first question *********************************************
 
-      button.addEventListener("click", () => {
-        button.classList.toggle("hide-button");
-        myAnswers.forEach((inp) => {
-          inp.checked = false;
+        // ********************************************* fetch the right answer *********************************************
+
+        theRightAnswer = myQuestionArr[randomQuestion]["rAsw"];
+
+        // ********************************************* End fetch the right answer *********************************************
+
+        // ********************************************* add event listener to result button for showing final score *********************************************
+
+        buttonResult.addEventListener("click", () => {
+          buttonResult.style.display = "none";
+          myAnswers.forEach((inp) => {
+            inp.disabled = true;
+          });
+          checkAnswer(myPickedAnswer, theRightAnswer);
         });
-        checkAnswer(myPickedAnswer, theRightAnswer);
-        myQuestionArr.splice(randomQuestion, 1);
-        if ((myQuestionArr.length = 0)) {
-          button.classList.add = "hide-button";
-          buttonResult.classList.remove = "hide-button";
-          buttonResult.addEventListener("click", () => {});
-        } else {
+        // ********************************************* End off add event listener to result button for showing final score *********************************************
+
+        // ********************************************* add event listener to next button for loading all the questions *********************************************
+
+        button.addEventListener("click", () => {
+          button.style.display = "none";
+          myAnswers.forEach((inp) => {
+            inp.checked = false;
+          });
+          checkAnswer(myPickedAnswer, theRightAnswer);
+          myQuestionArr.splice(randomQuestion, 1);
           let randomQuestions = Math.floor(
             Math.random() * myQuestionArr.length
           );
@@ -60,91 +76,44 @@ function getData() {
             );
           }
           theRightAnswer = myQuestionArr[randomQuestion]["rAsw"];
-          console.log(myQuestionArr.length);
-        }
-      });
-
-      myAnswers.forEach((inp) => {
-        inp.addEventListener("change", () => {
-          myPickedAnswer = inp.value;
-          myAnswers.forEach((inpp) => {
-            inpp.checked = false;
-          });
-          inp.checked = true;
-          button.classList.toggle("hide-button");
         });
-      });
-      function checkAnswer(answer, rightAnswer) {
-        if (answer === rightAnswer) {
-          console.log("correct");
-        } else {
-          console.log("wrong try again");
+
+        // ********************************************* End of add event listener to result button for showing final score *********************************************
+
+        // ********************************************* add event listener to check box for choosing right answer *********************************************
+
+        myAnswers.forEach((inp) => {
+          inp.addEventListener("change", () => {
+            myPickedAnswer = inp.value;
+            myAnswers.forEach((inpp) => {
+              inpp.checked = false;
+            });
+            inp.checked = true;
+            if (myQuestionArr.length === 1) {
+              buttonResult.style.display = "block";
+            } else {
+              button.style.display = "block";
+            }
+          });
+        });
+
+        // ********************************************* End of add event listener to check box for choosing right answer *********************************************
+
+        // ********************************************* Create function for checking answer and calculate final score  *********************************************
+
+        function checkAnswer(answer, rightAnswer) {
+          if (answer === rightAnswer) {
+            ++rightCount;
+          } else {
+            ++wrongCount;
+          }
         }
       }
-    })
+      // ********************************************* End of Create function for checking answer and calculate final score *********************************************
+    )
     .catch((error) => {
       console.log(error);
     });
 }
+
 getData();
-
-// async function getData() {
-//   try {
-//     let result = await fetch("QandA.json");
-//     let resultJson = await result.json();
-//     console.log(resultJson);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-
-// getData();
-
-// some changes
-
-// let input = document.createElement("input");
-// let lable = document.createElement("lable");
-// let inputValue = document.createAttribute("value");
-// inputValue.value = jsonResult["answers"][randomNumber];
-// input.setAttributeNode(inputValue);
-// let inputType = document.createAttribute("type");
-// inputType.value = "checkbox";
-// input.setAttributeNode(inputType);
-// let inputName = document.createAttribute("name");
-// inputName.value = "answers";
-// input.setAttributeNode(inputName);
-// let lableFor = document.createAttribute("for");
-// lableFor.value = jsonResult["answers"][randomNumber];
-// lable.setAttributeNode(lableFor);
-// let lableText = document.createTextNode(
-//   jsonResult["answers"][randomNumber]
-// );
-// lable.appendChild(lableText);
-// document.body.appendChild(input);
-// document.body.appendChild(lable);
-
-// for (let i = 0; i < 3; i++) {
-//         let randomNumber = Math.floor(
-//           Math.random() * jsonResult["answers"].length
-//         );
-//         if (randomQuetsion === randomNumber) {
-//           continue;
-//         } else {
-//           if ((myAnswers[randomNumber].innerHTML = "")) {
-//           }
-//         }
-//       }
-
-// let i = 0;
-// myAnswersLabel.forEach((asw) => {
-//   asw.innerHTML = jsonResult[randomQuestion][`asw${myArr[i]}`];
-//   myAnswers[i].setAttribute(
-//     "value",
-//     jsonResult[randomQuestion][`asw${myArr[i]}`]
-//   );
-//   ++i;
-// });
-
-// let gen = Math.floor(Math.random() * 4);
-// myAnswers[gen].setAttribute("value", jsonResult[randomQuestion]["rAsw"]);
-// myAnswersLabel[gen].innerHTML = jsonResult[randomQuestion]["rAsw"];
